687e907268e76877caf5bea8ca7a56d85b02af11
zscheel <zachary.scheel1@gmail.com>
2023-03-20 18:32:35 -0500

    added some more javascript to display new text

--- assets/script.js
+++ assets/script.js
@@ -2,21 +2,57 @@ var timeLeft = document.querySelector(".timer");
 var startButton = document.getElementById('start');
 var quiz = document.getElementById('quiz');
 var questionsContainer = document.getElementById('container');
-
+var questionElem = document.getElementById('question');
+var answerButtons = document.getElementById('answer-buttons');
+var shuffledQuestions, currentQuestionInd
 
 function startGame () {
  quiz.classList.add('hidden')
  questionsContainer.classList.remove('hidden')
+ shuffledQuestions = questions.sort(()=> Math.random() - .5)
+ currentQuestionInd = 0
  setNextQuestion();
 }
 
 function setNextQuestion () {
-
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionInd])
 }
+
+function showQuestion (question){
+questionElem.innerText = question.question;
+question.answers.forEach(answer => {
+    var button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+        button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtons.appendChild(button);
+})
+}
+
+function resetState() {
+    quiz.classList.add('hidden')
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+function  selectAnswer (e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtons.children)
+}
+
+
+
 
 var questions = [
     {
-        question: 'What is 2 + 2',
+        question: 'What is 2 + 2?',
         answers: [
            { text: '4', correct: true },
            { text: '22', correct: false },                 
